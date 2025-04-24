36e46877841d4a4b51329c7f501af469db1dee3c
Gilvan Cazumbá Cardoso <gmssaba@gmail.com>
2025-04-23 21:12:35 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,37 @@
+function escrever(texto){
+    return document.write("<br>"+ texto);
+}
+
+
+alert('Boas vinda, voce foi desafiado a descobrir um numero:');
+    var palpite = parseInt(prompt('Escolha um numero entre 0 e 10:'));
+    while(isNaN(palpite) || palpite < 0){
+        alert('Por favor digite um numero inteiro');
+        var palpite = parseInt(prompt('Escolha um numero entre 0 e 10:'));
+    }
+    var numeroSecreto = Math.floor(Math.random()*10);
+    var soma = 0;
+    soma += palpite
+    while(palpite != numeroSecreto){
+        if(palpite < numeroSecreto){
+            palpite = parseInt(prompt('É maior, escolha outro:'));
+            while(isNaN(palpite) || palpite < 0){
+                alert('Por favor digite um numerointeiro');
+                palpite = parseInt(prompt('É maior, escolha outro:'));
+            }
+            soma += palpite
+            
+        }else{
+            palpite = parseInt(prompt('É menor, escolha outro:'));
+            while(isNaN(palpite) || palpite < 0){
+                alert('Por favor digite um numerointeiro');
+                palpite = parseInt(prompt('É menor, escolha outro:'));
+            }
+            soma +=palpite
+        }
+
+    }
+    escrever('')
+    escrever('')
+    escrever(`Parabens voce acertou - o numero secreto foi: ${numeroSecreto}`);
+    escrever(`A soma dos seus palpites é ${soma}`);
