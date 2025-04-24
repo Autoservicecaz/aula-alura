9aff5358cbaa9cbd4d72a7b4cd9e399a761b34de
Gilvan Cazumbá Cardoso <gmssaba@gmail.com>
2025-04-23 23:29:31 -0300

    agora nao pode  por valores maiores que 10

--- app.js
+++ app.js
@@ -2,29 +2,29 @@ function escrever(texto){
     return document.write("<br>"+ texto);
 }
 
-
 alert('Boas vinda, voce foi desafiado a descobrir um numero:');
     var palpite = parseInt(prompt('Escolha um numero entre 0 e 10:'));
-    while(isNaN(palpite) || palpite < 0){
-        alert('Por favor digite um numero inteiro');
+    while(isNaN(palpite) || palpite < 0 || palpite > 10){
+        alert('Por favor digite um numero inteiro e que satisfaça a condição inicial [0,10]');
         var palpite = parseInt(prompt('Escolha um numero entre 0 e 10:'));
     }
+
     var numeroSecreto = Math.floor(Math.random()*10);
     var soma = 0;
     soma += palpite
     while(palpite != numeroSecreto){
         if(palpite < numeroSecreto){
             palpite = parseInt(prompt('É maior, escolha outro:'));
-            while(isNaN(palpite) || palpite < 0){
-                alert('Por favor digite um numerointeiro');
+            while(isNaN(palpite) || palpite < 0 || palpite > 10){
+                alert('Por favor digite um numerointeiro e que satisfaça a condição inicial [0,10]');
                 palpite = parseInt(prompt('É maior, escolha outro:'));
             }
             soma += palpite
             
         }else{
             palpite = parseInt(prompt('É menor, escolha outro:'));
-            while(isNaN(palpite) || palpite < 0){
-                alert('Por favor digite um numerointeiro');
+            while(isNaN(palpite) || palpite < 0 || palpite > 10){
+                alert('Por favor digite um numerointeiro e que satisfaça a condição inicial [0,10]');
                 palpite = parseInt(prompt('É menor, escolha outro:'));
             }
             soma +=palpite
